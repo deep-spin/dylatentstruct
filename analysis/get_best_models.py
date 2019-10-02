42f7f22c9f7998d4cf7775acf2a38b7c3153e147
Vlad Niculae <vlad@vene.ro>
2019-10-02 16:30:11 +0100

    get best model script

--- analysis/get_best_models.py
+++ analysis/get_best_models.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import shutil
 import glob
 import pandas
 
@@ -6,11 +8,34 @@ def main():
     fn = sys.argv[1]
     best = pandas.read_csv(fn)
 
+    print(best.columns)
+    script_f = open("run_test.sh", "w")
+
     for _, row in best.iterrows():
         print(row['attention'])
+        print(row['best_valid_acc'])
 
-        files = glob.glob(f"{row['fn_prefix']}*")
-        print(files)
+        files = sorted(list(glob.glob(f"{row['fn_prefix']}*{row['Run ID']}*")))
+
+        if len(files):
+            in_f = files[-1]
+            print(in_f)
+            out_f = sys.argv[2] + os.path.basename(in_f)
+            shutil.copy(in_f, out_f)
+            
+            print(f"""
+build/decomp --test
+--saved-model {out_f} \\
+--dataset {row['dataset']}
+--batch-size {row['batch_size']} \\
+--normalize-embed \\
+--drop 0 \\
+--attn {row['attention']} \\
+--sparsemap-eta {row['SM_eta']} \\
+--sparsemap-max-iter {int(row['SM_maxit'])} \\
+--sparsemap-active-set-iter {int(row['SM_ASET_maxit'])} \\
+--dynet-mem 2048;
+""", file=script_f)
 
 
 
